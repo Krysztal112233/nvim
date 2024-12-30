806770c40433926921be16324ce4cb667da428dc
Krysztal Huang <krysztal.huang@outlook.com>
2024-12-30 11:09:07 +0800

    Add mappings to `barbar`

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -36,7 +36,7 @@ return {
                 icons = {
                     -- Add diagnostics to tab bar
                     diagnostics = {
-                        [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+                        [vim.diagnostic.severity.ERROR] = { enabled = true },
                         [vim.diagnostic.severity.WARN] = { enabled = false },
                         [vim.diagnostic.severity.INFO] = { enabled = false },
                         [vim.diagnostic.severity.HINT] = { enabled = true },
@@ -50,6 +50,19 @@ return {
                     },
                 }
             }
+
+            local map = vim.keymap.set
+            local opts = { noremap = true, silent = true }
+
+            -- Move to previous/next
+            map({ "n", "v" }, '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+            map({ "n", "v" }, '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
+            -- Re-order to previous/next
+            map({ "n", "v" }, '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+            map({ "n", "v" }, '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+            map({ "n", "v" }, '<C-w>', '<Cmd>BufferClose<CR>', opts)
         end
     },
 
