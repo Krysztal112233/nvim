aad6e24403cfe28c38232d695eb15b28e9f85472
Krysztal Huang <krysztal.huang@outlook.com>
2025-04-23 17:53:39 +0800

    Tweak `lualine` for better view

--- lua/config/core/lualine.lua
+++ lua/config/core/lualine.lua
@@ -3,22 +3,24 @@ local M = {}
 function M.opts()
     local opts = {
         sections = {
-            lualine_c = {
-                {
-                    "navic",
-                    color_correction = nil,
-                    navic_opts = nil
-                },
-            },
+            lualine_c = { "lsp_status", { "navic", }, },
             lualine_x = {
-                'lsp_status',
+                "filetype",
+                { "fileformat", symbols = { unix = "UNIX", dos = "WINDOWS", mac = "MAC", } },
+                "encoding"
+            },
+            lualine_y = {
+                "progress", "location"
+            },
+            lualine_z = {
+                { "datetime", style = "default" }
             }
         },
         options = {
             theme = "auto",
             globalstatus = true,
             disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
-            extensions = { "quickfix" }
+            extensions = { "fzf", "neo-tree" }
         },
     }
     return opts
