4654c82ef2f776c0b9abbbec45767fd6be13008b
Krysztal Huang <krysztal.huang@outlook.com>
2025-04-24 09:46:44 +0800

    Remove `nvim-java`.
    The experience of using it is very bad

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -22,43 +22,6 @@ return {
         opts = require("config.core.language.clangd_ext").opts,
     },
 
-    -- Java support
-    {
-
-        "nvim-java/nvim-java",
-        dependencies = {
-            "nvim-java/lua-async-await",
-            "nvim-java/nvim-java-refactor",
-            "nvim-java/nvim-java-core",
-            "nvim-java/nvim-java-test",
-            "nvim-java/nvim-java-dap",
-            "MunifTanjim/nui.nvim",
-            "neovim/nvim-lspconfig",
-            "mfussenegger/nvim-dap",
-            {
-                "williamboman/mason.nvim",
-                opts = {
-                    registries = {
-                        "github:nvim-java/mason-registry",
-                        "github:mason-org/mason-registry",
-                    },
-                },
-            },
-            {
-                "williamboman/mason-lspconfig.nvim",
-                opts = {
-                    handlers = {
-                        ["jdtls"] = function()
-                            require("java").setup()
-                            require("lspconfig").jdtls.setup() -- Add this line for using the wrapped jdtls.setup
-                        end,
-                    },
-                },
-            },
-        },
-        opts = {},
-    },
-
     -- Just support
     {
         "IndianBoy42/tree-sitter-just",
