73af6bf3c08831cbae66aa7efcde1d8ad37c6dff
Krysztal Huang <krysztal.huang@outlook.com>
2025-06-22 22:10:24 +0800

    Use `tmp` directory as `nvim-jdtls` workspace

--- lua/config/core/nvim-jdtls.lua
+++ lua/config/core/nvim-jdtls.lua
@@ -7,10 +7,14 @@ function M.opts()
     local pkg_jar = vim.fn.glob(vim.fs.joinpath(java_test_path, "java-test", "extension", "server") ..
         "/*.jar");
 
+    local tmp_file = os.tmpname()
+    local workspace = vim.fs.joinpath(vim.fn.fnamemodify(tmp_file, ":h"), "jdtls",
+        vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'))
     return {
         cmd = {
             "jdtls",
             ("--jvm-arg=-javaagent:%s"):format(lombok_path),
+            ("--data:%s"):format(workspace),
         },
 
         root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
