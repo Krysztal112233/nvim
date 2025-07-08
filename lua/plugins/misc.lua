22f38b7329a4c4c53dd938c06843345fe7569c5a
Krysztal Huang <krysztal.huang@outlook.com>
2025-07-09 01:18:39 +0800

    Remove `kanagawa` colorscheme

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,11 +1,4 @@
 return {
     { "folke/tokyonight.nvim" },
     { "loctvl842/monokai-pro.nvim" },
-
-    {
-        "rebelot/kanagawa.nvim",         -- neorg needs a colorscheme with treesitter support
-        config = function()
-            vim.cmd.colorscheme("kanagawa")
-        end,
-    },
 }
