ff35b27b90046c7fe658f69e33fa0426a331cd46
Krysztal Huang <krysztal.huang@outlook.com>
2026-01-04 20:30:05 +0800

    Add `vtsls` support

---
+++ lua/plugins/lsp-vtsls.lua
@@ -0,0 +1,20 @@
+return {
+  'yioneko/nvim-vtsls',
+  config = function()
+    require('lspconfig.configs').vtsls = require('vtsls').lspconfig
+    require('lspconfig').vtsls.setup {
+      settings = {
+        typescript = {
+          inlayHints = {
+            parameterNames = { enabled = 'literals' },
+            parameterTypes = { enabled = true },
+            variableTypes = { enabled = true },
+            propertyDeclarationTypes = { enabled = true },
+            functionLikeReturnTypes = { enabled = true },
+            enumMemberValues = { enabled = true },
+          },
+        },
+      },
+    }
+  end,
+}
