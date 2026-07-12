61a95f78d9b4aae8d2349dee07cd84b1c86a1beb
Krysztal Huang <krysztal.huang@outlook.com>
2026-07-12 23:16:34 +0800

    Add macro recording status `statusline` and dependencies

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,9 +1,28 @@
+local function macro_recording()
+  local register = vim.fn.reg_recording()
+  if register == '' then
+    return ''
+  end
+
+  return ('REC @%s · completion paused'):format(register)
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
   opts = {
     sections = {
+      lualine_a = {
+        'mode',
+        {
+          macro_recording,
+          cond = function()
+            return vim.fn.reg_recording() ~= ''
+          end,
+          color = { fg = '#ffffff', bg = '#e64553', gui = 'bold' },
+        },
+      },
       lualine_c = { 'lsp_status' },
       lualine_x = {
         'filetype',
@@ -23,6 +42,22 @@ return {
       globalstatus = true,
       disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'ministarter', 'snacks_dashboard' } },
       extensions = { 'neo-tree' },
+      refresh = {
+        events = {
+          'WinEnter',
+          'BufEnter',
+          'BufWritePost',
+          'SessionLoadPost',
+          'FileChangedShellPost',
+          'VimResized',
+          'Filetype',
+          'CursorMoved',
+          'CursorMovedI',
+          'ModeChanged',
+          'RecordingEnter',
+          'RecordingLeave',
+        },
+      },
     },
   },
 }
